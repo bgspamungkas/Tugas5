220991c232579c9d7d5ee125fa340b80e4f6e2e1
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-11-10 00:50:08 +0700

    Update Latihan_3.py

--- Latihan/Latihan_3.py
+++ Latihan/Latihan_3.py
@@ -1,26 +1,24 @@
-print('03')
 def faktorial(n):
-    faktorialN=1
-    while(n>0):
-        faktorialN=faktorialN*n
-        n-=1
-    return faktorialN
-def kombinasi(a,b):
-    c=a-b
-    hasil=faktorial(a)/(faktorial(b)*faktorial(c))
+    faktorialn = 1
+    while( n > 0):
+        faktorialn = faktorialn * n
+        n -= 1
+    return faktorialn
+def kombinasi( a , b ):
+    c = a - b
+    hasil = faktorial(a) / (faktorial(b) * faktorial(c))
     print(hasil)
-def permutasi(a,b):
-    c=a-b
-    hasil=faktorial(a)/faktorial(c)
+def permutasi( a , b ):
+    c = a - b
+    hasil = faktorial(a) / faktorial(c)
     print(hasil)
 
-print('3a')
-#C=kombinasi
+print('a')
 a=5
 b=3
 kombinasi(a,b)
-print('3b')
-#P=permutasi
+
+print('b')
 x=10
 y=7
 permutasi(x,y)
