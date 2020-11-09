743bc4b91f68ba9744a07cffe429de993ff747a5
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-11-10 00:50:03 +0700

    Update Latihan_2.py

--- Latihan/Latihan_2.py
+++ Latihan/Latihan_2.py
@@ -1,71 +1,73 @@
-print('02')
-#starFormation1
+#formasistar1
 def starFormation1(n):
-    kolom=0
-    baris=n
+    
+    kolom = 0
+    baris = n
 
-    i=0
-    while(i<=n):
-        j=0
-        while(j<kolom):
+    i = 0
+    while(i <= n):
+        j = 0
+        while(j < kolom):
             print('*',end='')
-            j+=1
+            j += 1
         print('')
-        i+=1
-        kolom+=1
+        i += 1
+        kolom += 1
 
 #contoh1
 starFormation1(4)
 print()
 
-#starFormation2
+#formasistar2
 def starFormation2(n):
-    kolom=n
-    baris=n
+    
+    kolom = n
+    baris = n
 
-    i=0
-    while(i<=n):
-        j=0
-        while(j<kolom):
+    i = 0
+    while(i <= n):
+        j = 0
+        while(j < kolom):
             print('*',end='')
-            j+=1
+            j += 1
         print('')
-        i+=1
-        kolom-=1
+        i += 1
+        kolom -= 1
         
 #contoh2
 starFormation2(4)
 print()
 
-#starFormation3
+#formasistar3
 def starFormation3(n):
-    kolom=0
-    baris=n
-    puncak=(n+1)/2
+    
+    kolom = 0
+    baris = n
+    puncak = (n+1)/2
 
-    i=0
-    while(i<=n):
-        j=0
-        while(j<kolom):
+    i = 0
+    while(i <= n):
+        j = 0
+        while(j < kolom):
             print('*',end='')
-            j+=1
+            j += 1
         print()
-        i+=1
-        kolom+=1
+        i += 1
+        kolom += 1
 
         if(kolom == puncak):
-            kolom=puncak
-            baris=puncak
+            kolom = puncak
+            baris = puncak
 
-            i=0
-            while(i<=n):
-                j=1
-                while(j<=kolom):
+            i = 0
+            while(i <= n):
+                j = 1
+                while(j <= kolom):
                     print('*',end='')
-                    j+=1
+                    j += 1
                 print()
-                i+=1
-                kolom-=1
+                i += 1
+                kolom -= 1
                 
 #contoh3
 starFormation3(7)
