65c32860ade90843036101ff81a2305a7b90b861
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-11-10 00:49:28 +0700

    Create statistik.py

---
+++ Latihan/statistik.py
@@ -0,0 +1,30 @@
+print('04')
+print('4a')
+def sum(*n):
+    i=0
+    for x in n:
+        i=i+x
+    print(i)
+print('4b')
+def average(*n):
+    i=0
+    j=0
+    for x in n:
+        i=i+x
+        j+=1
+        average=i/j
+    print(average)
+print('4c')
+def max(*n):
+    max=n[0]
+    for i in (n):
+        if(i>max):
+            max=i
+    print(max)
+print('4d')
+def min(*n):
+    min=n[0]
+    for i in (n):
+        if(i<min):
+            min=i
+    print(min)
