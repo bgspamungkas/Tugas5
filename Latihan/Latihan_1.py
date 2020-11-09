6b72210c11c4799e1cd159935ef4c4abd42528aa
Bagus Prihandono Pamungkas <71998969+bgspamungkas@users.noreply.github.com>
2020-11-10 00:49:59 +0700

    Update Latihan_1.py

--- Latihan/Latihan_1.py
+++ Latihan/Latihan_1.py
@@ -1,15 +1,20 @@
-print('01')
 def isPythagoras (a,b,c):
-    if((a*a)==(c*c)-(b*b) or (b*b)==(c*c)-(a*a) or (c*c)==(a*a)+(b*b)):
+    if((a * a) == ( c * c )-( b * b )
+       or ( b * b ) == ( c * c )-( a * a )
+       or ( c * c ) == ( a * a )+( b * b )):
         print(True)
     else:
         print(False)
 
-print('1a')
+
+print('a')
 isPythagoras(3,4,5)
-print('1b')
+
+print('b')
 isPythagoras(5,9,12)
-print('1c')
+
+print('c')
 isPythagoras(8,6,10)
-print('1d')
+
+print('d')
 isPythagoras(7,8,11)
